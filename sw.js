70b4c10dd217d10f1aecba4261a9f9686b11ed29
Yu <yu0975248749@gmail.com>
2026-09-11 21:35:28 +0800

    public snapshot update (行動裝置月計畫彈出面板與勾選排版修正)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'c-form-cache-66d08202494f';
+const CACHE_NAME = 'c-form-cache-d507e3c701f9';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
